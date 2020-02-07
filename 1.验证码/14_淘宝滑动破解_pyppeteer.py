0ea10ddfddb90921a7075b09a36180ed457a0b1c
mander-he <2424681270@qq.com>
2020-02-07 21:53:34 +0800

    Update 14_淘宝滑动破解_pyppeteer.py
    代码缩进格数有误

--- 1.验证码/14_淘宝滑动破解_pyppeteer.py
+++ 1.验证码/14_淘宝滑动破解_pyppeteer.py
@@ -63,7 +63,6 @@ async def main(username, pwd, url):# 定义main协程函数，
                 print('确保账户安全重新入输入')
                 # 程序退出。
                 loop.close()
-              else:
                 print(page.url)
                 await get_cookie(page)
     #time.sleep(100)
